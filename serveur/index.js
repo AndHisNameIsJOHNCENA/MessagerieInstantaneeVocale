d57a60c07970078c3ce1a9046ae91bb3436b70ca
RhiobeT <rhiobet@gmail.com>
2015-12-04 05:54:32 +0100

    LE CHAT QUI FONCTIONNE

--- serveur/index.js
+++ serveur/index.js
@@ -1,9 +1,50 @@
-var http = require('http');
+const PORT = 8080;
+const DB_URL = "mongodb://localhost:27017/db";
+const COLLECTIONS = ["messages"];
 
-const PORT=8080;
+var http = require('http'),
+	fs = require('fs'),
+	mongojs = require('mongojs'),
+	moment = require('moment');
+
+var db = mongojs(DB_URL, COLLECTIONS);
 
 function handleRequest(request, response) {
-	response.end('It works !! YAY Et c\'est ' + request.url);
+	fs.readFile('chat.html', function(err, html) {
+		if (err) {
+			throw err;
+		}
+		if (request.method === "POST") {
+			var body = '';
+			request.on('data', function(data) {
+				body += data;
+			});
+			request.on('end', function() {
+				db.messages.save({date: Date.now(), msg: body.substring(4)});
+			});
+		}
+
+		response.writeHeader(200, {'Content-Type': 'text/html'});
+		
+		db.messages.find().sort({date: 1}, function(err, messages) {
+			if (err || messages == []) {
+				console.log(err);
+				html = html.toString().replace('<div id="chat">',
+						'<div id="chat">'
+							+ '<p>Aucun message !</p>\n');
+			} else {
+				html = html.toString();
+				messages.forEach(function(message) {
+					html = html.replace('<div id="chat">',
+							'<div id="chat">'
+								+ '<p>' + moment(message.date).format('HH:mm:ss')
+								+ ' | ' + message.msg + '</p>');
+				});
+			}
+			response.write(html);
+			response.end();
+		});
+	});
 }
 
 var server = http.createServer(handleRequest);
